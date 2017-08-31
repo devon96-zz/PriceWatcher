d0b55b7725f3c39708fbb3410e97317f9afd1a3e
Konrad Dryja <kdryja@gmail.com>
2017-08-31 12:18:18 +0100

    Added emailing feature

--- post_request.py
+++ post_request.py
@@ -1,41 +1,38 @@
-# import requests
-# import json
-
-# url = "https://be.wizzair.com/7.0.0/Api/search/search"
-# payload = {"isFlightChange":False,"isSeniorOrStudent":False,"flightList":[{"departureStation":"ABZ","arrivalStation":"GDN","departureDate":"2017-12-16"}],"adultCount":1,"childCount":0,"infantCount":0,"wdc":True,"rescueFareCode":""}
-# headers = {'content-type': 'application/json'}
-
-# response = requests.post(url, data=json.dumps(payload), headers=headers)
-
-# jsonResponse = json.loads(response.text)
-# flights = jsonResponse["outboundFlights"]
-
-# f = open("currentPrice.txt", "r+")
-# currentPrice = float(f.read())
-# f.close()
-
-# adminPrice = flights[0]["fares"][0]["administrationFeePrice"]["amount"]
-# discBasePrice = flights[0]["fares"][0]["discountedFarePrice"]["amount"]
-
-# fullPrice =  (float(adminPrice) + float(discBasePrice))
-
-# if fullPrice != currentPrice:
-#     f = open("currentPrice.txt", "w")
-#     f.write(str(fullPrice))
-#     f.close()
-#     print ("PRICE CHANGED")
-
+import requests
+import json
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email("test@example.com")
-to_email = Email("test@example.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, subject, to_email, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
+url = "https://be.wizzair.com/7.0.0/Api/search/search"
+payload = {"isFlightChange":False,"isSeniorOrStudent":False,"flightList":[{"departureStation":"ABZ","arrivalStation":"GDN","departureDate":"2017-12-16"}],"adultCount":1,"childCount":0,"infantCount":0,"wdc":True,"rescueFareCode":""}
+headers = {'content-type': 'application/json'}
+
+response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+jsonResponse = json.loads(response.text)
+flights = jsonResponse["outboundFlights"]
+
+f = open("currentPrice.txt", "r+")
+currentPrice = float(f.read())
+f.close()
+
+adminPrice = flights[0]["fares"][0]["administrationFeePrice"]["amount"]
+discBasePrice = flights[0]["fares"][0]["discountedFarePrice"]["amount"]
+
+fullPrice =  (float(adminPrice) + float(discBasePrice))
+
+if fullPrice != currentPrice:
+    f = open("currentPrice.txt", "w")
+    f.write(str(fullPrice))
+    f.close()
+    print("PRICE CHANGED")
+
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("WizzairPriceChange@konrad.com")
+    to_email = Email("kdryja@gmail.com")
+    subject = "WizzAir ABZ -> GDN Alert"
+    message = "Changed from: " + currentPrice + " GBP to: " + fullPrice + " GBP."
+    content = Content("text/plain", message)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
